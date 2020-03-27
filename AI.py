ba830817ffe62373d3821a3dc0cd22ea430d868e
Sjomullen <54435499+Sjomullen@users.noreply.github.com>
2020-03-27 08:09:21 +0100

    Add files via upload

--- AI.py
+++ AI.py
@@ -30,32 +30,30 @@ def preprocess(img):
 def transform_reward(reward):
     return np.sign(reward)
 
-def fit_batch(model, gamma, start_states, actions, rewards, next_states, is_terminal):
-                """Do one deep Q learning iteration.
+def fit_batch(model, target_model, gamma, start_states, actions, rewards, next_states, is_terminal):
+    """do one deep Q learining iteration.
+
+    Params:
+    - model: The DQN
+    - target_model: The target DQN
+    - gamma: Discount factor (should be 0.99)
+    - start_states: numpy array of starting states
+    - actions: numpy array of one-shot encoded actions corresponding to the start states
 
 
-Params:
-- model: The DQN
-- gamma: Discount factor (should be 0.99)
-- start_states: numpy array of starting states 
-- actions: numpy array of one-hot encoded actions corresponding to the start states
-- rewards: numpy array of rewards corresponding to the start and actions
-- next_states: numpy array of the resulting states corresponding to the start states and actions
-- is_terminal: numpy boolean array of wether the resulting state is terminal
 
+    """
 
-"""
-
-
-# First, predict the Q values of the next states. Note we are passing one as the mask.
-next_Q_values = model.predict([next_states, np.ones(action.shape)])
-# The Q values of the terminal states is 0 by definition, so override them
-next_Q_values[is_terminal] = 0
-# The Q values of each start state is the reawrd + gamma * the max next state of Q value
-Q_values = rewards + gamma * np.max(next_Q_values, axis=1)
-# Fit the keras model. Note how we are passing the actions as the mask and mulitplying 
-# the targets by the actions.
-model.fit(
+    # First, predict the Q values of the next states. Note how we are passing ones as the mask.
+    next_Q_values = target_model.predict([next_states, np.ones(actions.shape)])
+    # The Q values of the terminal states is 0 by definition, so override them
+    next_Q_values[is_terminal] = 0
+    # The Q values of each start state is the reward + gamma * the max next state Q value
+    # The Q values of each start state is the reawrd + gamma * the max next state of Q value
+    Q_values = rewards + gamma * np.max(next_Q_values, axis=1)
+    # Fit the keras model. Note how we are passing the actions as the mask and mulitplying 
+    # the targets by the actions.
+    model.fit(
     [start_states, actions], actions * Q_values[:, None],
     nb_epoch=1, batch_size=len(start_states), verbose=0
 
@@ -98,8 +96,77 @@ def atari_model(n_actions):
 
 class RingBuf:
     def _init_(self, size):
-        
+        # Pro-tip: When implementing a ring buffer, always allocate one extra element, This way, self.start == self.end always means  the buffer is EMPTY, whereas if you alloacte exactly the right number of elemets, it could also mean the buffer is full. This greatly simplifies the rest of the code.
+        self.data = [None] * (size + 1)
+        self.end = 0
+
+    def append(self, element):
+        self.data[self.end] = element
+        self.end = (self.end + 1) % len(self.data)   
+        #end == start and yet we just added one element. This means the buffer has one
+        # too many element. Remove the first element by incrementing start.
+        if self.end == self.start:
+            self.start = (self.start + 1) % len(self.data)
+
+        def __getitem__(self, idx):
+            return self.data[(self.start + idx) % len(self.data)]
+
+        def __len__(self):
+            if self.end < self.start:
+                return self.end + len(self.data) - self.start
+
+            else:
+                return self.end - self.start
+
+            def __iter__(self):
+                for i in range(len(self)):
+                    yield self[i]
+
+             
+def q_iteration(env, model, state, iteration, memory):
+    # Choose epsilon based on the iteration
+    epsilon = get_epsilon_for_iteration(iteration)
+
+    # Choose action
+    if random.random() < epsilon:
+        action = env.action_space.sample()
+    else:
+        action = choose_best_action(model, state)
+
+        # Play one game iteration (note: according to the next paper, you should actually play 4 times here)
+        new_frame, reward, is_done, _ = env.step(action)
+        memory.add(state, action, new_frame, reward, is_done)
+
+    # Sample and fit
+    batch = memory.sample_batch(32)
+    fit_batch(model, batch)
+
+def copy_model(model):
+    """Returns a copy of a keras model."""
+    model.save('tmp_model')
+    return keras.models.load_model('tmp_model')
 
 
+def huber_loss_simple(a, b):
+    error = a - b
+
+    if abs(error) > 1.0:
+        return abs(error) - 1/2
+    return error*error / 2
+
+
+from keras import backend as K 
+# Note: pass in_keras=False to use this function with raw numbers of numpy arrays for testing
+
+def huber_loss(a, b, in_keras=True):
+    error = a - b
+    quadratic_term = error*error / 2
+    linear_term = abs(error) - 1/2
+    use_linear_term = (abs(error) > 1.0)
+
+    if in_keras:
+        # Keras won't let us multiply floats by booleans, so we explicitly cast the booleans to floats
+         use_linear_term = K.cast(use_linear_term, 'float32')
+    return use_linear_term * linear_term + (1-use_linear_term) * quadratic_term
 
 
