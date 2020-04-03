a185d1f8f68136421ec2ee43af936da42e5e8bee
Sjomullen <54435499+Sjomullen@users.noreply.github.com>
2020-04-03 13:21:35 +0200

    Add files via upload

--- AI.py
+++ AI.py
@@ -1,33 +1,29 @@
-import numpy
+import numpy as np 
+import matplotlib.pyplot as plt
 
-def NN(m1, m2, w1, w2, b):
-    z = m1 * w1 + m2 * w2 + b
-    return sigmoid(z)
+#  length, width, type/color
+data = [[3,    1.5,  1],
+        [2,    1,    0],
+        [4,    1.5,  1],
+        [3,    1,    0],
+        [3.5,  0.5,  1],
+        [2,    0.5,  0],
+        [5.5,  1,    1],
+        [1,    1,    0]]
+
+mystery_flower = [4.5, 1]
+
+w1 = np.random.rand()
+w2 = np.random.rand()
+b = np.random.rand()
 
 def sigmoid(x):
-    return 1/(1 + numpy.exp(-x))
+    return(1/1 + np.exp(-x))
 
-w1 = numpy.random.randn()
-w2 = numpy.random.randn()
-b =  numpy.random.randn()
+T = np.linspace(-5, 5, 10)
+Y = sigmoid(T)
+Y
 
-n = NN(3, 1.5, w1, w2, b)
- 
-# For training loop
-def cost(b):
-    return(b - 4) ** 2
+plt.plot(T, Y)
 
-def num_slope(b):
-    h = 0.0001
-    return(cost(b+h) - cost(b))/h
-
-def slope(b):
-    return 2 * (b - 4) 
-
-b = 8 
-  
-#"Training loop"
-for i in range(100): 
-    b = b - .1 * slope(b)
-    print(b)
-
+plt.show()
